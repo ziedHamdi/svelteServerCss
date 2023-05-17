f2851e139396de2370f4b67ae9d6d2749380c1c3
Zied Hamdi <zhamdi@gmail.com>
2023-05-17 10:12:34 +0200

    showcasing how css is not loaded

---
+++ src/routes/css/[templateId]/+server.js
@@ -0,0 +1,16 @@
+export async function GET({ params }) {
+	const { templateId } = params;
+	let cssContent
+	console.log('responding to request for templateId : ', params);
+		cssContent = `
+html, body {
+    background-color: blue;
+}
+div {
+  border: 1px solid gray;
+}
+`;
+	// Return the CSS content as a response
+
+	return new Response(cssContent, { headers: { 'Content-Type': 'text/html; charset=utf-8' } });
+}
